f360315181a871dc9e621db0856c518065aa7f13
Lokesh Das <ldd3g@mst.edu>
2025-08-05 03:34:01 +0000

    feat: implement merge sort from scratch

--- Src/algo-basics/sorting/merge_sort.cpp
+++ Src/algo-basics/sorting/merge_sort.cpp
@@ -6,26 +6,27 @@ void merge(vector <int> &arr, int st, int mid, int end){
     vector <int> temp;
     int right = st, left = mid + 1;
 
-    while( right < mid && left < end ){
+    while( right <= mid && left <= end ){
         if(arr[right] <= arr[left]){
             temp.push_back(arr[right]);
             right++;
         }else {
             temp.push_back(arr[left]);
+            left++;
         }
     }
 
-    while(right < mid){
+    while(right <= mid){
         temp.push_back(arr[right]);
         right++;
     }
 
-    while(right > mid){
+    while(left <= end){
         temp.push_back(arr[left]);
         left++;
     }
 
-    for(int i = 0; i < temp.size() - 1; i++){
+    for(int i = 0; i < temp.size(); i++){
         arr[st + i] = temp[i];
     }
 }
@@ -37,15 +38,16 @@ void mergeSort(vector <int> &arr, int st, int end){
         mergeSort(arr, st, mid);
         mergeSort(arr, mid + 1, end);
 
-        merge(arr, st, mid, end)
+        merge(arr, st, mid, end);
     }
 }
 
 int main(){
-    vector<int> test = [12, 32, 45, 6, 76, 1, 9];
+    vector<int> test = {12, 32, 45, 6, 76, 1, 9};
 
     mergeSort(test, 0, test.size() - 1);
     for (int num: test){
-        cout << num << endl;
+        cout << num <<  " ";
     }
+    cout << endl;
 }
